fd1cbdb7d67ea84fa335eff2e015e6af6f3e2ccd
xieshentao <xieshentao@yidianhulian.com>
2020-08-13 10:41:37 +0800

    vue

--- api/config/database.php
+++ api/config/database.php
@@ -21,13 +21,13 @@ return [
             // 数据库类型
             'type'              => env('database.type', 'mysql'),
             // 服务器地址
-            'hostname'          => env('database.hostname', '127.0.0.1'),
+            'hostname'          => env('database.hostname', '47.102.197.28'),
             // 数据库名
-            'database'          => env('database.database', ''),
+            'database'          => env('database.database', 'iblog'),
             // 用户名
             'username'          => env('database.username', 'root'),
             // 密码
-            'password'          => env('database.password', ''),
+            'password'          => env('database.password', '@xyh939369942'),
             // 端口
             'hostport'          => env('database.hostport', '3306'),
             // 数据库连接参数
